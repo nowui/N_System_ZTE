42e4cd758743c748a87f2bf25e25ef3e9ec9ddd8
Zhong Yong Qiang <258003787@qq.com>
2016-02-24 01:19:14 +0800

    2016-02-24 01:19

--- N_System_ZTE_Web/src/main/webapp/js/gis.js
+++ N_System_ZTE_Web/src/main/webapp/js/gis.js
@@ -3,14 +3,20 @@ var scene = null;
 var htmlUrl = document.location.host;
 //判断网页的打开方式是本地打开还是通过网络打开
 //不同的打开方式url赋值不同
-//var htmlUrl = "http://10.45.26.42:8090";
-var htmlUrl = "http://122.96.155.202:18129";
+var flyManager = null;
+var flyRoutes = null;
+var flyRoute = null;
+var routeLoaded = false;
+var htmlUrl = "http://10.45.26.42:8090";
+//var htmlUrl = "http://122.96.155.202:18129";
+var serverUrl = "http://10.45.34.178:8080";
 var sceneUrl= htmlUrl + "/iserver/services/3D-RealspaceSample/rest/realspace";
+//var sceneUrl= htmlUrl + "/iserver/services/3D-zhixinghai/rest/realspace";
 
 function onPageLoad() {
     //初始化三维场景控件
 
-    //sceneControl = new SuperMap.Web.UI.Controls.SceneControl($get("map"), initCallback, failedCallback);
+    sceneControl = new SuperMap.Web.UI.Controls.SceneControl($get("map"), initCallback, failedCallback);
 
 
 }
@@ -28,16 +34,67 @@ function initCallback() {
 	scene.open(sceneAddress, sceneName);
 	//获取场景中的图层
 
- 	//var bound = new SuperMap.Bounds(116.203953, 39.523575, 116.212675, 39.535796);
-   //  scene.get_flyingOperator().flyToBounds(bound);
+	//var bound = new SuperMap.Bounds(93.13015833333333, 39.786830555555554, 93.13015833333333, 39.786830555555554);
+	//scene.get_flyingOperator().flyToBounds(bound);
+
+	flyManager = scene.get_flyManager();
+	setFilePath();
+
+	return;
 
     //获取Realspace控件的场景，控件和场景是一对一的捆绑关系
     scene = sceneControl.get_scene();
 
+	var myAction = new SuperMap.Web.UI.Action3Ds.DrawPlacemarkAction(sceneControl);
+	sceneControl.set_sceneAction(myAction);
 
+	//setTimeout("countSec()", 1000)
+
+	//setFilePath();
 }
 
 //控件初始化失败后的回调函数
 function failedCallback() {
     alert("Realspace initialized failed!");
+}
+
+//获取飞行文件路径
+function getFilePath() {
+    return  serverUrl+"/fx1.fpf";  //读取飞行文件
+}
+
+function setFilePath() {
+    if (flyManager != null) {
+        if (flyManager.get_flyStatus() == SuperMap.Web.Realspace.FlyStatus.FSTOP) {
+            //获取飞行路线
+            flyRoutes = flyManager.get_routes();
+            if (flyRoutes != null) {
+                var filePath = getFilePath();
+                if (flyRoutes.fromFile(filePath) == false) {
+                    return false;
+                }
+
+                flyRoute = flyRoutes.get_currentRoute();
+
+                routeLoaded = true;
+            }
+        }
+    } else {
+        return false;
+    }
+}
+
+function play() {
+    //判断路径是否已加载。
+    if (routeLoaded) {
+        if (flyManager == null && flyRoutes == null &&
+flyManager.get_flyStatus() == SuperMap.Web.Realspace.FlyStatus.FPLAY) {
+            return;
+        }
+        else {
+
+            flyManager.play();
+
+        }
+    }
 }
